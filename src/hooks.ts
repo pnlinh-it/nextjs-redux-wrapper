d7149c14034f58991d6858a4836e4a3bea6e27fa
Linh pham <pnlinh.it@gmail.com>
2024-04-20 13:44:05 +0700

    Use redux-toolkit 2

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,14 @@
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from 'react-redux'
+// import type { TypedUseSelectorHook } from 'react-redux'
 import type { AppState, AppDispatch, AppStore } from './store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
 export const useAppStore: () => AppStore = useStore
+
+// https://react-redux.js.org/using-react-redux/usage-with-typescript#withtypes
+// If we use react-redux 9
+// export const useAppSelector = useSelector.withTypes<AppState>()
+// export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// export const useAppStore = useStore.withTypes<AppStore>()
--- src/store.ts
+++ src/store.ts
@@ -6,14 +6,16 @@ import { pokemonApi } from '@/src/api';
 // https://github.com/kirill-konshin/next-redux-wrapper/blob/8.x/packages/demo-redux-toolkit/store.ts
 const combineReducer = combineReducers({
   // Use user as key instead of [userSlice.name] for better suggestion
-  user: userSlice.reducer,
-  pokemonApi: pokemonApi.reducer
+  [userSlice.name]: userSlice.reducer,
+  [pokemonApi.reducerPath]: pokemonApi.reducer
 });
 
-const reducer = (state: any, action: any): ReturnType<typeof combineReducer> => {
+// const reducer = (state: any, action: any): ReturnType<typeof combineReducer> => {
+const reducer: typeof combineReducer = (state, action: any) => {
   console.log(action)
   console.log(state)
   if (action.type === HYDRATE) {
+    // @ts-ignore
     return {
       ...state,
       ...action.payload,
@@ -31,6 +33,6 @@ const makeStore = () => configureStore({
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore['getState']>;
-type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
 export type AppDispatch = AppStore['dispatch']
 export const wrapper = createWrapper<AppStore>(makeStore, {debug: true});
