0540c51d89accf9aa87baaeab3f16457ca291459
Linh pham <pnlinh.it@gmail.com>
2024-04-20 14:01:16 +0700

    Use tookit

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,12 +3,13 @@ import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from 'react-
 // import type { TypedUseSelectorHook } from 'react-redux'
 import type { AppState, AppDispatch, AppStore } from './store'
 
-export const useAppDispatch = () => useDispatch<AppDispatch>
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
 export const useAppStore: () => AppStore = useStore
+
+// export const useAppDispatch = () => useDispatch<AppDispatch>
+// export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
 
 // https://react-redux.js.org/using-react-redux/usage-with-typescript#withtypes
 // If we use react-redux 9
-// export const useAppSelector = useSelector.withTypes<AppState>()
-// export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<AppState>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 // export const useAppStore = useStore.withTypes<AppStore>()
--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,23 @@
-import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { Action, combineSlices, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { createWrapper, HYDRATE } from '@/src/wrapper';
 import { userSlice } from '@/src/features/user.slice';
 import { pokemonApi } from '@/src/api';
 
 // https://github.com/kirill-konshin/next-redux-wrapper/blob/8.x/packages/demo-redux-toolkit/store.ts
-const combineReducer = combineReducers({
-  // Use user as key instead of [userSlice.name] for better suggestion
-  [userSlice.name]: userSlice.reducer,
-  [pokemonApi.reducerPath]: pokemonApi.reducer
-});
+// const combineReducer = combineReducers({
+//   // Use user as key instead of [userSlice.name] for better suggestion
+//   [userSlice.name]: userSlice.reducer,
+//   [pokemonApi.reducerPath]: pokemonApi.reducer
+// });
+
+const combineReducer = combineSlices(userSlice, pokemonApi)
+
+// https://github.com/reduxjs/redux-templates/blob/master/packages/cra-template-redux-typescript/template/src/app/store.ts
+export type AppState = ReturnType<typeof combineReducer>
 
 // const reducer = (state: any, action: any): ReturnType<typeof combineReducer> => {
-const reducer: typeof combineReducer = (state, action: any) => {
+// const reducer: typeof combineReducer = (state, action: any) => {
+const reducer = (state: any, action: any) => {
   console.log(action)
   console.log(state)
   if (action.type === HYDRATE) {
@@ -32,7 +38,6 @@ const makeStore = () => configureStore({
 });
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type AppState = ReturnType<AppStore['getState']>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
 export type AppDispatch = AppStore['dispatch']
 export const wrapper = createWrapper<AppStore>(makeStore, {debug: true});
